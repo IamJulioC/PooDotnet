042232fa6baa836885ca11218525bf295d352b48
Julio <iamjulioc@live.com>
2025-03-19 23:59:04 -0300

    Poo V.1.0.0

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokiac3 = new Nokia("123456789", "Nokia C300", "123456789", 16);
+Iphone iphone11 = new Iphone("987654321", "Iphone 11", "987654321", 64);
+
+nokiac3.Ligar();
+nokiac3.InstalarAplicativo("WhatsApp");
+
+iphone11.Ligar();
+iphone11.InstalarAplicativo("Instagram");
